71f2af00e7f2e36c54afd722f6f7786a80080004
Ömercan <86682297+omercankara@users.noreply.github.com>
2023-02-21 17:14:12 +0300

    herokuApp

---
+++ db.js
@@ -0,0 +1,11 @@
+const config = {
+    db:{
+        host: "us-cdbr-east-06.cleardb.net",
+        user: "b3969dbf30acb3",
+        password: "ecfdeba1",
+        database: "gameblog"
+    }
+
+}
+
+module.exports = config
--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express")
-
+var cors = require('cors')
+bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 
 const app = express()
 app.use(express.json())
-app.get("/",(req,res) => {
-    res.send("Hello World")
+
+
+app.get("/", async (req, res) => {
+    try {
+        let result = await db.execute("SELECT * FROM user")
+        res.json(result[0])
+
+    } catch (err) {
+        res.sendStatus(500) //Status SERVER FAİLED
+    }
 })
 
 app.listen(process.env.PORT || 5000, () => {
