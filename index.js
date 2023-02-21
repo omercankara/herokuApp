b877d0185267c9f73ad486b0668702504a96bcfd
Ömercan <86682297+omercankara@users.noreply.github.com>
2023-02-21 17:28:28 +0300

    Heroku

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 const express = require("express")
 const app = express()
 var cors = require('cors')
+
 bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 
 
-app.use(express.json())
+const userRoutes = require("./routes/user")
+app.use(userRoutes)
 
 
-app.get("/", async (req, res) => {
-    try {
-        let result = await db.execute("SELECT * FROM user")
-        res.json(result[0])
 
-    } catch (err) {
-        res.sendStatus(500) //Status SERVER FAİLED
-    }
-})
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("server")
---
+++ routes/user.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const router = express.Router()
+const db = require("../db")
+
+
+
+router.get("/", async (req, res) => {
+    try {
+        let result = await db.execute("SELECT * FROM user")
+        res.json(result[0])
+        console.log(result[0])
+
+    } catch (err) {
+        res.sendStatus(500) //Status SERVER FAİLED
+    }
+})
+
+
+module.exports = router
