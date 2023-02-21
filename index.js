907d321c8723f1e5177886fca1933a4f412a77fa
Ömercan <86682297+omercankara@users.noreply.github.com>
2023-02-21 18:30:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express")
 const app = express()
 
-
 var cors = require('cors')
 bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
+app.use(express.json())
 
 
 
