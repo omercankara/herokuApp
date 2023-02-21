032a3cd3be31d88fc6dfc5cb67ab4b556493a4a6
Ömercan <86682297+omercankara@users.noreply.github.com>
2023-02-21 18:36:45 +0300

    Update db.js

--- data/db.js
+++ data/db.js
@@ -4,12 +4,12 @@ const config = require("../databaseconnect/config")
 let connection = mysql.createConnection(config.db)
 
 connection.connect(function (err) {
-    if (err) {
+  /*   if (err) {
         return console.log(err);
     }
     connection.query("select * from user", function (err, result) {
         console.log(result);
-    })
+    }) */
 })
 
 module.exports = connection.promise()
